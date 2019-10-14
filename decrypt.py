9941884cbdf139dd62ee875ae731bce59108d523
spdv123 <tianshen477@live.com>
2019-10-14 16:04:43 +0800

    Add decrypt directory recursively

--- decrypt.py
+++ decrypt.py
@@ -1,6 +1,16 @@
+# coding=utf-8
+from __future__ import print_function
 import os
 import argparse
 import glob
+import fnmatch
+
+
+def valid(input_path):
+    file = open(input_path, 'rb')
+    magic = file.read(12)
+    return magic == b'bogehcollege'
+
 
 def decrypt(input_path):
     path, file = os.path.split(input_path)
@@ -12,17 +22,38 @@ def decrypt(input_path):
         fname = file_split[0]
         ext = '.' + file_split[1]
     output_path = os.path.join(path, fname + '_decrypt' + ext)
-    os.system('decrypt {} {}'.format(input_path, output_path))
+    if not valid(input_path):
+        print('[x] Not an encrypted file: {}'.format(input_path))
+        return
+    if os.name == 'posix':  # Unix system need prefix
+        os.system('./decrypt {} {}'.format(input_path, output_path))
+    else:
+        os.system('decrypt {} {}'.format(input_path, output_path))
 
-parser = argparse.ArgumentParser()
-parser.add_argument('file', help='Path of file or directory to be decrypted', type=str)
-p = parser.parse_args()
-path = p.file
-if os.path.isfile(path):
-    decrypt(p.file)
-else:
-    if not path.endswith(os.path.sep):
-        path += os.path.sep
-    flist = glob.glob(path + '*.png')
-    for f in flist:
-        decrypt(f)
+
+def find_files(directory, pattern):
+    for root, dirs, files in os.walk(directory):
+        for basename in files:
+            if fnmatch.fnmatch(basename, pattern):
+                filename = os.path.join(root, basename)
+                yield filename
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'file', help='Path of file or directory to be decrypted', type=str)
+    p = parser.parse_args()
+    path = p.file
+    if os.path.isfile(path):
+        decrypt(p.file)
+    else:
+        if not path.endswith(os.path.sep):
+            path += os.path.sep
+        flist = find_files(path, '*.png')
+        for f in flist:
+            decrypt(f)
+
+
+if __name__ == '__main__':
+    main()
